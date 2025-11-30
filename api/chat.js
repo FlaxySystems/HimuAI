8acece7a9dff66d2fb23f93c054d99d560e6c3e9
FlaxySystems | Himanshu Gurjar <umbraixhosting@gmail.com>
2025-11-30 17:34:07 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
     res.status(200).json(data);
 
   } catch (error) {
+    console.error("Server error:", error);
     res.status(500).json({ error: "Server error" });
   }
 }
